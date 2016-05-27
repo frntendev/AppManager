f027cac8b093e0337bd7b522e96b12ade1d99151
Sepehr <Sepehr.aliakbari71@gmail.com>
2016-05-27 13:46:01 +0430

    First Commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+var myApp = angular.module('myApp',['myApp.filters', 'myApp.services', 'myApp.directives']);
+myApp.
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/index',
+        controller: IndexCtrl
+      }).
+      when('/addPost', {
+        templateUrl: 'partials/addPost',
+        controller: AddPostCtrl
+      }).
+      when('/readPost/:id', {
+        templateUrl: 'partials/readPost',
+        controller: ReadPostCtrl
+      }).
+      when('/editPost/:id', {
+        templateUrl: 'partials/editPost',
+        controller: EditPostCtrl
+      }).
+      when('/deletePost/:id', {
+        templateUrl: 'partials/deletePost',
+        controller: DeletePostCtrl
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+    $locationProvider.html5Mode(true);
+  }]);
